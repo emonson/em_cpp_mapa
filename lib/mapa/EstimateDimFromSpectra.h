a6d90de85b89ad72eb2371db4aa914787ac5b77d
Eric E Monson <emonson@cs.duke.edu>
2014-05-06 10:30:23 -0400

    getting further in EstDim C++, but still has bugs

--- lib/mapa/EstimateDimFromSpectra.h
+++ lib/mapa/EstimateDimFromSpectra.h
@@ -3,7 +3,8 @@
 
 
 #include <Eigen/Core>
-#include <stdlib.h>
+#include <iostream>     // std::cout
+#include <algorithm>    // std::min
 
 using namespace Eigen;
 
@@ -11,19 +12,233 @@ using namespace Eigen;
 namespace MAPA {
 
 class EstimateDimFromSpectra {
+// function vStats = EstimateDimFromSpectra( cDeltas, S_MSVD, alpha0 )
+// %
+// % Estimates intrinsic dimensionality and good scales given the multiscale singular values
+// %
+// % IN:
+// %   cDeltas         : (#scales) vector of scales
+// %   S_MSVD          : (#scales)*(#dimensions) matrix of singular values: the (i,j) entry is the j-th singular value of a cell at scale cDeltas(i) around a point
+// %
+// % OUT:
+// %   vStats          : structure containing the following fields:
+// %                       DimEst      : estimate of intrinsic dimensionality
+// %                       GoodScales  : vector of indices (into the vector cDeltas) of good scales used for dimension estimate
+// %
+// 
 
 public:
 
     EstimateDimFromSpectra()
     {
-			DimEst = -1;
-			GoodScalesLow = -1;
-			GoodScalesHigh = -1;
+        DimEst = -1;
+        GoodScalesLow = -1;
+        GoodScalesHigh = -1;
+        width = -1;
     };
     
-    void EstimateDimensionality(const ArrayXd &cDelta, ArrayXXd &S_MSVD, double alpha0)
+    void EstimateDimensionality(const ArrayXd &cDelta, const ArrayXXd &S_MSVD, double alpha0 = 0.2)
     {
-    	
+        double slope;
+        ArrayXd current_to_prev_spectra_diff;
+        ArrayXd largest_to_prev_spectra_diff;
+        double mean_fractional_spectrum_rise;
+        
+        int lowerScaleIdx;
+        int upperScaleIdx;
+        
+        // [nScales,nDims] = size(S_MSVD);
+        // 
+        // scale_distances = cDeltas;
+        // 
+        // if nargin<3; alpha0 = 0.2; end
+        // 
+        // width = 5;
+        // lowerScaleIdxMax = min(12,nScales);
+        // upperScaleIdxMax = nScales;
+        // 
+        // vStats.GoodScales = [1,nScales];
+        // vStats.DimEst = nDims;
+        
+        int nScales = S_MSVD.rows();
+        int nDims = S_MSVD.cols();
+        
+        scale_distances = cDelta;
+        spectra = S_MSVD;
+        
+        width = 5;
+        
+        // NOTE: remember using zero-based indices unlike Matlab 1-based !!
+        int lowerScaleIdxMax = std::min(12-1, nScales-1);
+        int upperScaleIdxMax = nScales-1;
+        GoodScalesLow = 1-1;
+        GoodScalesHigh = nScales-1;
+        // NOTE: Watch this one – Don't know if should keep it more intuitive, or switch to zero-based for use as index...?
+        // Keeping it as intuitive dimensionality value, so need to subtract one when using it as an index...!!
+        DimEst = nDims;
+
+        // Start with the smallest singular values, which should be the most noisy
+        // and loop through decreasing dim index down to 1, and break out early if
+        // we hit a dim at which the slope never flattens out below 0.1 at any
+        // r/delta
+        
+        // for dim = nDims:-1:1,
+        
+        for (int dim = nDims-1; dim >= 0; dim--)
+        {
+             
+        //     vStats.DimEst = dim;
+        //     spectrum = S_MSVD(:,dim);
+            
+            // Note: difference between dim as index and DimEst as dimensionality!
+            DimEst = dim + 1;
+            
+            // find a lower bound for the optimal scale
+            // try to find an r/delta index at which the slope drops below 0.1
+            // (flattens out)
+        
+        //     for ii = width:lowerScaleIdxMax+1,
+        //         lowerScaleIdx = ii;
+        //         slope = compute_slope(scale_distances, spectrum, width, lowerScaleIdx);
+        //         if (slope < 0.1),
+        //             break;
+        //         end
+        //     end
+            
+            for (int ii = width - 1; ii <= lowerScaleIdxMax+1; ii++)
+            {
+                lowerScaleIdx = ii;
+                slope = compute_slope(dim, ii);
+                if (slope < 0.1)
+                {
+                    break;
+                }
+            }
+
+            // Special case for the first (highest dim, noisiest singular values)
+            // Testing whether slope never flattened out (or only flattened out on the
+            // last iteration. If it didn't flatten out (go < 0.1), soon enough, 
+            // then it's "not a noisy singular value" and don't
+            // need to check when the flat started to rise again.
+            // This would also mean we don't have to test any lower dim. All scales are
+            // "good", and dimensionality of the manifold is the full dimensionality of
+            // the system.
+        
+        //     if (dim == nDims) && (lowerScaleIdx == lowerScaleIdxMax+1),
+        //         return;
+        //     end
+        
+            if ((dim == nDims-1) && (lowerScaleIdx == lowerScaleIdxMax+1))
+            {
+                return;
+            }
+        
+            // find an upper bound for the optimal scale
+            // Special case for first iteration, no test for slope <= alpha0 for
+            // some reason.
+            // If the slope never even dropped below alpha0
+            // then don't bother trying to find another rise from
+            // the nonexistent flat portion, and break out of this loop because it's
+            // assumed once the slope always stays above alpha0, none of the rest of
+            // the dims are "noisy", i.e. those larger singular value dims are all
+            // part of the manifold around this net point, so we can set the
+            // estimation of the dimension, and we won't get any more help from
+            // these dims judging the window of good scales (r/delta indices)
+        
+        //     if (dim == nDims) || (slope <= alpha0)
+            
+            if ((dim == nDims-1) || (slope <= alpha0))
+            {
+
+                // Set j either at iMax+1 or at the r/delta index at which the slope
+                // flattened out below 0.1
+
+        //         for jj = lowerScaleIdx:upperScaleIdxMax,
+        //             upperScaleIdx = jj;
+        //             slope = compute_slope(scale_distances, spectrum, width, upperScaleIdx);
+        //             if (slope > alpha0),
+        //                 break;
+        //             end
+        //         end
+        //         upperScaleIdx = upperScaleIdx - 1;
+
+                for (int jj = lowerScaleIdx; jj <= upperScaleIdxMax; jj++)
+                {
+                    upperScaleIdx = jj;
+                    slope = compute_slope(dim, jj);
+                    if (slope > alpha0)
+                    {
+                        break;
+                    }
+                }
+                
+                // Only compute gap after first iteration
+        
+        //         if(dim < nDims),
+        
+                if (dim < nDims-1)
+                {
+                
+                    // If the curve flattens out for a while, but the gap between it and
+                    // the previous dim spectrum is wide enough, going to count it as a
+                    // "non-noisy" real dim that's part of the manifold
+        
+        //             idx_window = upperScaleIdx-width+1:upperScaleIdx;
+        //             current_to_prev_spectra_diff = (spectrum(idx_window)-S_MSVD(idx_window,dim+1));
+        //             largest_to_prev_spectra_diff = (S_MSVD(idx_window,1)-S_MSVD(idx_window,dim+1));
+        //             mean_fractional_spectrum_rise = mean(current_to_prev_spectra_diff./largest_to_prev_spectra_diff);
+                    
+                    int lo = upperScaleIdx-width+1;
+                    int hi = upperScaleIdx;
+                    current_to_prev_spectra_diff = spectra.col(dim-1).segment(lo,hi) - spectra.col(dim+1-1).segment(lo,hi);
+                    largest_to_prev_spectra_diff = spectra.col(1-1).segment(lo,hi) - spectra.col(dim+1-1).segment(lo,hi);
+                    mean_fractional_spectrum_rise = (current_to_prev_spectra_diff / largest_to_prev_spectra_diff).mean();
+                    
+                    
+        //             if mean_fractional_spectrum_rise > 0.2
+        //                 % real manifold dim, so leave everything the same and return
+        //                 return;
+        //             end
+                    
+                    if (mean_fractional_spectrum_rise > 0.2)
+                    {
+                        // real manifold dim, so leave everything the same and return
+                        return;
+                    }
+
+        //         end
+                
+                }
+
+                // still a noisy scale with a low-enough flat portion that is
+                // helping us find the "good scales", so update those. 
+                // We want to use the values from the last dim found to be
+                // "noisy", i.e. flat in some portion and not risen enough from
+                // the previous, higher, dim, so keep updating GoodScales each
+                // time, so when we break out of the loop with a noisy == false
+                // will have the proper r/delta indices recoreded
+        
+        //         vStats.GoodScales = [lowerScaleIdx-1 upperScaleIdx];
+                
+                GoodScalesLow = lowerScaleIdx-1;
+                GoodScalesHigh = upperScaleIdx;
+
+        //     else
+            }
+            else
+            {
+        
+        // slope never dropped below alpha0, leave everything the same and return
+        
+        //         return;
+
+        //     end
+            }
+
+        // end
+        }
+        
+        return;
     };  
 
     int GetDimension()
@@ -49,15 +264,27 @@ private:
     int GoodScalesHigh;
     ArrayXd scale_distances;
     ArrayXXd spectra;
+    int width;
     
-	double compute_slope(int dim, int idx, int width)
+	double compute_slope(int dim, int idx)
 	{
 		ArrayXd spectrum = spectra.col(dim);
-		double slope;
+		double slope, numerator, denominator;
 		
+        // function slope = compute_slope(distances, spectrum, width, idx)
+        //     s1 = distances(idx-width+1:idx);
+        //     sp = spectrum(idx-width+1:idx);
+        //     slope = (sum(s1.*sp) - sum(s1)*sum(sp)/numel(s1)) / (sum(s1.^2)-sum(s1)^2/numel(s1));
+        // end
+        
 		ArrayXd s1 = scale_distances.segment(idx-width+1, width);
 		ArrayXd sp = spectrum.segment(idx-width+1, width);
 		// slope = (sum(s1.*sp) - sum(s1)*sum(sp)/numel(s1)) / (sum(s1.^2)-sum(s1)^2/numel(s1));
+		
+		numerator = (s1 * sp).sum() - (s1.sum()*sp.sum()/(double)s1.size());
+		denominator = s1.square().sum() - (s1.sum()*s1.sum())/(double)s1.size();
+		
+		slope = numerator / denominator;
 		
 		return slope;
 	};
--- tools/mapa_pieces_test/estdim_test.cpp
+++ tools/mapa_pieces_test/estdim_test.cpp
@@ -9,6 +9,7 @@ int main(int argc, char * argv[])
     double alpha0 = 0.3;
     MAPA::EstimateDimFromSpectra estdim;
     
+    // -------------------------------------
     // Read in distances
     Eigen::ArrayXd Delta_1;
     std::cout << "Reading in Artifical test data (rev1) seed 1 scale distances" << std::endl;
@@ -27,10 +28,34 @@ int main(int argc, char * argv[])
 
     estdim.EstimateDimensionality(Delta_1, NetS_1, alpha0);
     
-    std::cout << estdim.GetDimension() << std::endl;
-    std::cout << estdim.GetLowerScaleIdx() << std::endl;
-    std::cout << estdim.GetUpperScaleIdx() << std::endl;
+    std::cout << "dim = 2, " << estdim.GetDimension() << std::endl;
+    std::cout << "lo = 8, " << estdim.GetLowerScaleIdx() << std::endl;
+    std::cout << "hi = 15, " << estdim.GetUpperScaleIdx() << std::endl;
 
+
+    // -------------------------------------
+    // Read in distances
+    Eigen::ArrayXd Delta_2;
+    std::cout << "Reading in Artifical test data (rev1) seed 1 scale distances" << std::endl;
+    igl::readDMAT( "/Users/emonson/Programming/em_cpp_mapa/data/artdat_rev1_estdim_delta2.dmat", Delta_2 );
+    
+    // Read in spectra
+    Eigen::ArrayXXd NetS_2;
+    std::cout << "Reading in seed 1 spectra for test data (rev1)" << std::endl;
+    igl::readDMAT( "/Users/emonson/Programming/em_cpp_mapa/data/artdat_rev1_estdim_s2.dmat", NetS_2);
+		
+        // i_seed == 2 (matlab idx)
+        //     GoodScales: [9 33]
+        //         DimEst: 1
+
+    estdim.EstimateDimensionality(Delta_2, NetS_2, alpha0);
+    
+    std::cout << "dim = 1, " << estdim.GetDimension() << std::endl;
+    std::cout << "lo = 8, " << estdim.GetLowerScaleIdx() << std::endl;
+    std::cout << "hi = 32, " << estdim.GetUpperScaleIdx() << std::endl;
+
+
+    // -------------------------------------
     // Read in distances
     Eigen::ArrayXd Delta_60;
     std::cout << "Reading in Artifical test data (rev1) seed 60 scale distances" << std::endl;
@@ -47,9 +72,9 @@ int main(int argc, char * argv[])
 
     estdim.EstimateDimensionality(Delta_60, NetS_60, alpha0);
     
-    std::cout << estdim.GetDimension() << std::endl;
-    std::cout << estdim.GetLowerScaleIdx() << std::endl;
-    std::cout << estdim.GetUpperScaleIdx() << std::endl;
+    std::cout << "dim = 3, " << estdim.GetDimension() << std::endl;
+    std::cout << "dim = 0, " << estdim.GetLowerScaleIdx() << std::endl;
+    std::cout << "dim = 49, " << estdim.GetUpperScaleIdx() << std::endl;
 
     return 0;
 }
