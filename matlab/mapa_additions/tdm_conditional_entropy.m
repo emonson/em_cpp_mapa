98dcf63527e79012fcff33db82215a5c8748e4cf
Eric E Monson <emonson@cs.duke.edu>
2014-06-03 09:57:15 -0400

    adding redsvd into repository for now, but not sure if I will end up using it. should test against eigen random svd and svdlibc for large sparse matrices

--- matlab/mapa_additions/tdm_conditional_entropy.m
+++ matlab/mapa_additions/tdm_conditional_entropy.m
@@ -1,5 +1,7 @@
 function h2 = tdm_conditional_entropy(tdm)
 
+% S. Lafon 2006 uses a "conditional entropy" cutoff instead of the
+% "standard" entropy that I'd assumed Mauro was talking about.
 % Calculating "conditional entropy"
 sum2 = sum(tdm,2);
 ratio2 = tdm./repmat(sum2,[1 size(tdm,2)]);
